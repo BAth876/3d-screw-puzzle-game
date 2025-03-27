92f3999aaad93300939c725c0289373e4f7f9620
Bler Four <your.email@example.com>
2025-03-27 17:28:26 +0000

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-  output: 'standalone',
-  images: {
-    unoptimized: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/',
-          outputPath: 'static/',
-        },
-      },
-    });
-    return config;
-  }
+  swcMinify: true,
+  output: 'standalone'
 };
 
 module.exports = nextConfig; 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Game } from '../src/components/Game'
 
 export default function Home() {
